70109e3a1f64b9e27d49c7843006ece662a05156
nkshr11 <nov27umbrella@gmail.com>
2017-11-18 18:29:30 +0900

    Move data.py to price_handler/base.py. Move HistoricCSVDataHandler in data.py to price_handler/yahoo_daily_csv_bar.py. Refactor an abstract data_handler class.

--- price_handler/yahoo_daily_csv_bar.py
+++ price_handler/yahoo_daily_csv_bar.py
@@ -1,48 +1,4 @@
-import datetime
-import os, os.path
-import pandas as pd
-
-from abc import ABCMeta, abstractmethod
-
-from price_parser import PriceParser
-from event import MarketEvent
-
-class DataHandler(object):
-    """
-    DataHandler is an abstract base class providing an interface for
-    all subsequent (inherited) data handlers (both live and historic).
-
-    The goal of a (derived) DataHandler object is to output a generated
-    set of bars (OLHCVI) for each symbol requested.
-
-    This will replicate how a live strategy would function as current
-    market data would be sent "down the pipe". Thus a historic and live
-    system will be treated identically by the rest of the backtesting suite.
-    """
-
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def get_latest_bars(self, symbol, N=1):
-        """
-        Returns the last N bars from the latest_symbol list,
-        or fewer if less bars are available
-        :param symbol:
-        :param N:
-        :return:
-        """
-        raise NotImplementedError("should implement get_latest_bars()")
-
-    @abstractmethod
-    def update_bars(self):
-        """
-        Pushes the latest bar to the latest symbol structure
-        for all symbols in the symbol list.
-        :return:
-        """
-        raise NotImplementedError("Shhould implement update_bars()")
-
-class HistoricCSVDataHandler(DataHandler):
+class YahooCSVDataHandler(DataHandler):
     """
     HistoricCSVDataHandler is designed to read CSV files for
     each requested symbol from disk and provide an interface
